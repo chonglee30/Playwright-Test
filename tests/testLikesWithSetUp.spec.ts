5f259d785d07bb0f137a736cc72097ef9f8440e7
Chong S. Lee <chonglee30@gmail.com>
2025-07-21 14:42:06 -0700

    fix: add retry and assert title

--- tests/testLikesWithSetUp.spec.ts
+++ tests/testLikesWithSetUp.spec.ts
@@ -1,12 +1,14 @@
 import { test, expect } from '@playwright/test';
-
-test('check Likes Counter Number', async ({ page }) => {
-  console.log('Step3: Check Likes Counter')
-  await page.goto('/');
-  await page.getByText('Global Feed').click()
-
-  const likesButton = page.locator('app-favorite-button button').first()
-  await expect(likesButton).toContainText('0')
-  await likesButton.click()
-  await expect(likesButton).toContainText('1')
+test.describe('Test Likes' , () => {
+test.describe.configure({ retries: 3 });
+  test('check Likes Counter Number', async ({ page }) => {
+    console.log('Step3: Check Likes Counter')
+    await page.goto('/');
+    await page.getByText('Global Feed').click()
+    await expect(page.locator('app-article-preview', {hasText: "Likes Test3 Title"})).toBeVisible()
+    const likesButton = page.locator('app-favorite-button button').first()
+    await expect(likesButton).toContainText('0')
+    await likesButton.click()
+    await expect(likesButton).toContainText('1')
+  })
 })
