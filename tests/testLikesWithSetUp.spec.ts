5d1e1e3feb32cdacdfc24e192b41378095e3be5b
Chong S. Lee <chonglee30@gmail.com>
2025-07-31 17:15:44 -0700

    feat: able to search article title

--- tests/testLikesWithSetUp.spec.ts
+++ tests/testLikesWithSetUp.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { waitForCompleteLoading } from '../utils/common-waiting';
+import { binarySearch } from '../utils/arrayDataCheck';
 
 test.describe('Test Likes' , () => {
   test('check Likes Counter Number', async ({ page }) => {
@@ -7,6 +8,11 @@ test.describe('Test Likes' , () => {
     await page.goto('/');
     await page.getByText('Global Feed').click()
     await waitForCompleteLoading(page);
+
+    const articles = await page.locator('app-article-list .article-preview h1').allTextContents()
+    articles.sort();
+    const rowIndex = binarySearch(articles,  process.env.PROJECT_TITLE as string)
+    expect (rowIndex).not.toBe(-1);
    
     await expect(page.locator('app-article-preview', {hasText: `${process.env.PROJECT_TITLE}`})).toBeVisible()
     const articleLink = page.locator(`a[href="/article/${process.env.PROJECT_SLUGID}"]`);
--- utils/arrayDataCheck.ts
+++ utils/arrayDataCheck.ts
@@ -1,3 +1,20 @@
+export function binarySearch(array: string[], target: string): number {
+    let low =0;
+    let high = array.length-1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high)/2);
+        if (array[mid] === target) return mid
+
+        if (array[mid]> target) {
+            high = mid -1
+        } else { // array[mid]> target
+            low = mid +1
+        }
+    }
+    return -1   
+}
+
 export function isArrayDataUnique(arrayData: any[]): boolean {
     const set = new Set(arrayData)
     return set.size === arrayData.length;
