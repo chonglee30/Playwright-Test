6118403ffcd48b4f57d12809939579db4ed37a2f
Chong S. Lee <chonglee30@gmail.com>
2025-08-03 16:16:48 -0700

    fix: common waiting order

--- utils/common-waiting.ts
+++ utils/common-waiting.ts
@@ -2,6 +2,6 @@ import {Page, expect} from '@playwright/test'
 
 export async function waitForCompleteLoading(page: Page) {
   let allArticlesResponse = await page.waitForResponse('**/api/articles?limit=10&offset=0');
-  expect(allArticlesResponse.status()).toBe(200)
   await expect(page.locator(':text("Loading articles...")')).toBeHidden()
+  expect(allArticlesResponse.status()).toBe(200)
 }
