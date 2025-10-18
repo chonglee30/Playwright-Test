309d8486906f7a2e6736853a5f43face438936ce
Chong S. Lee <chonglee30@gmail.com>
2025-10-18 13:12:03 -0700

    feat: user registration tests

--- tests/testUserRegister.spec.ts
+++ tests/testUserRegister.spec.ts
@@ -1,31 +1,68 @@
 import { expect, test } from "@playwright/test";
 
-test.describe('Registration Test', () => {
-  [
-    {username: "ab", errorMsg: "username is too short (minimum is 3 characters)", isErrorMsgDisplayed: true},
-    {username: "abc", errorMsg: "username", isErrorMsgDisplayed: false},
-    {username: "abcdeabcdeabcdeabcde", errorMsg: "username", isErrorMsgDisplayed: false},
-    {username: "abcdeabcdeabcdeabcde1", errorMsg: "username is too long (maximum is 20 characters)", isErrorMsgDisplayed: true}
-  ].forEach(({ username, errorMsg, isErrorMsgDisplayed}) => {
-    test(`Test length of username ${username} to display error msg`, async({page}) => {
-      await page.goto('/') 
-      await page.getByText('Sign up').click()
+import * as registrationInputValues from '../test-data/registration.json'
 
-      const heading = page.getByRole('heading', {name: 'Sign up'});
-      await expect(heading).toBeVisible()
-      await expect(heading).toHaveText('Sign up')
+test.describe('User Sign up Page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+    await page.getByText('Sign up').click()
 
-      await page.getByPlaceholder('Username').fill(username)
-      await page.getByPlaceholder('Email').fill('test')
-      await page.getByPlaceholder('Password').fill('Playwright')
-      await page.getByRole('button', {name: 'Sign up'}).click() 
-
-      if (isErrorMsgDisplayed) {
-       await expect(page.locator('.error-messages')).toContainText(errorMsg)
-      } else {
-        await expect(page.locator('.error-messages')).not.toContainText(errorMsg)
-      }
-    })
+    const heading = page.getByRole('heading', { name: 'Sign up' });
+    await expect(heading).toBeVisible()
+    await expect(heading).toHaveText('Sign up')
   })
 
+  test.describe('Username Required Length Tests', () => {
+    const usernameInput = registrationInputValues.username;
+    usernameInput.forEach(({ username, errorMsg, isErrorMsgDisplayed }) => {
+      test(`Required length of username ${username} and ensure display error msg if violate requirement`, async ({ page }) => {
+        await page.getByPlaceholder('Username').fill(username)
+        await page.getByPlaceholder('Email').fill('test')
+        await page.getByPlaceholder('Password').fill('Playwright')
+        await page.getByRole('button', { name: 'Sign up' }).click()
+
+        if (isErrorMsgDisplayed) {
+          await expect(page.locator('.error-messages')).toContainText(errorMsg)
+        } else {
+          await expect(page.locator('.error-messages')).not.toContainText(errorMsg)
+        }
+      })
+    })
+  });
+
+  test.describe('Email Format Tests', () => {
+    const emailInput = registrationInputValues.email;
+    emailInput.forEach(({ email, errorMsg, isErrorMsgDisplayed }) =>{
+      test(`Email ${email} format Test and error msg if violate requirement`, async ({ page }) => {
+        await page.getByPlaceholder('Username').fill('ab')
+        await page.getByPlaceholder('Email').fill(email)
+        await page.getByPlaceholder('Password').fill('seahawks')
+        await page.getByRole('button', { name: 'Sign up' }).click()
+
+        if (isErrorMsgDisplayed) {
+          await expect(page.locator('.error-messages')).toContainText(errorMsg)
+        } else {
+          await expect(page.locator('.error-messages')).not.toContainText(errorMsg)
+        }
+      })
+    })
+  });
+
+  test.describe('Password Required Length Tests', () => {
+    const passwordInput = registrationInputValues.password;
+    passwordInput.forEach(({ password, errorMsg, isErrorMsgDisplayed }) => {
+      test(`Required length of password ${password} and ensure display error msg if violate requirement`, async ({ page }) => {
+        await page.getByPlaceholder('Username').fill('abc')
+        await page.getByPlaceholder('Email').fill('test')
+        await page.getByPlaceholder('Password').fill(password)
+        await page.getByRole('button', { name: 'Sign up' }).click()
+
+        if (isErrorMsgDisplayed) {
+          await expect(page.locator('.error-messages')).toContainText(errorMsg)
+        } else {
+          await expect(page.locator('.error-messages')).not.toContainText(errorMsg)
+        }
+      })
+    })
+  });
 });
